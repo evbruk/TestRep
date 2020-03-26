0cffcefba5b10f08f58231caa27f2ef688f7ae67
evbruk <eva.bruklich@gmail.com>
2020-03-26 16:21:44 -0400

    first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+
+public class Test {
+
+    public static void main(String[] args)
+    {
+        int x = 8;
+        int y = 3;
+        System.out.println(x + y);
+    }
+}
